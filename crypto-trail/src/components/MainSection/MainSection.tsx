88a0575ec205571ea15fc7b317863bf355cea76e
r1derPUSH <riderworkable1234@gmail.com>
2025-10-23 00:17:40 +0300

    Created hash-routes

--- crypto-trail/src/components/MainSection/MainSection.tsx
+++ crypto-trail/src/components/MainSection/MainSection.tsx
@@ -1,12 +1,16 @@
 import "../MainSection/mainSection.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
+import Overview from "./Overview/Overview";
+import Footer from "./Footer/Footer";
 
 function MainSection() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/footer" element={<Footer />} />
       </Routes>
     </HashRouter>
   );
