90ef28c8034dac0f2aff9073f13e126bb650b211
r1derPUSH <riderworkable1234@gmail.com>
2025-10-23 00:13:49 +0300

    Created more components

--- crypto-trail/src/components/MainSection/MainSection.tsx
+++ crypto-trail/src/components/MainSection/MainSection.tsx
@@ -1,7 +1,15 @@
 import "../MainSection/mainSection.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
 
 function MainSection() {
-  return <div></div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default MainSection;
