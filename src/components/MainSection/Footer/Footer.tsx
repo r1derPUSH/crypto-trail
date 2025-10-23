8f7ec57409efea35972227340ac449b9807c7786
r1derPUSH <riderworkable1234@gmail.com>
2025-10-23 22:04:22 +0300

    Created HomeButton component

--- src/components/MainSection/Footer/Footer.tsx
+++ src/components/MainSection/Footer/Footer.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Footer.css";
+import InvestButtonFooter from "./InvestButton/InvestButtonFooter";
+import PortfolioButton from "./PortfolioButton/PortfolioButton";
+import HomeButton from "./HomeButton/HomeButton";
 
 function Footer() {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <HomeButton />
+      <InvestButtonFooter />
+      <PortfolioButton />
+    </footer>
+  );
 }
 
 export default Footer;
---
+++ src/components/MainSection/Footer/HomeButton/HomeButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeButton() {
+  return (
+    <div className="home-button">
+      <span>Home</span>
+    </div>
+  );
+}
+
+export default HomeButton;
