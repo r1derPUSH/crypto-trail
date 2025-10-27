3419f917da675869cb95ab30f05f8471473c01f5
r1derPUSH <riderworkable1234@gmail.com>
2025-10-27 21:28:42 +0200

    Created HashRouter path for register section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
 import MainSection from "./components/MainSection/MainSection";
 
 function App() {
-  return <MainSection />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<MainSection />}></Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
