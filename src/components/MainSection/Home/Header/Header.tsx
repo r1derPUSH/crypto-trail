e799583c86f4cad7826cbf5d0fd540c6ea8976fe
r1derPUSH <riderworkable1234@gmail.com>
2025-10-29 00:07:59 +0200

    Created component for change theme btn

--- src/components/MainSection/Home/Header/Header.tsx
+++ src/components/MainSection/Home/Header/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AvatarSection from "./AvatarSection/AvatarSection";
 import SearchBar from "./SearchBar";
+import ChangeTheme from "./ChangeTheme/ChangeTheme";
 
 function Header() {
   return (
     <div>
       <AvatarSection />
       <SearchBar />
+      <ChangeTheme />
     </div>
   );
 }
