93a395be0c7fea64c57226ea2ec9154064ef2790
r1derPUSH <riderworkable1234@gmail.com>
2025-10-28 23:08:48 +0200

    Added styles to user-img

--- src/components/MainSection/Home/Header/AvatarSection/AvatarSection.tsx
+++ src/components/MainSection/Home/Header/AvatarSection/AvatarSection.tsx
@@ -1,10 +1,11 @@
 import { FiChevronRight } from "react-icons/fi";
 import "./AvatarSection.css";
+import manExample from "../img/man-example.jpg";
 
 function AvatarSection() {
   return (
     <div className="avatar-container">
-      <img src="" alt="Avatar" />
+      <img className="user-img" src={manExample} alt="Avatar" />
       <span className="username">username</span>
       <FiChevronRight className="arrow-right-icon" />
     </div>
