c36c286d280c28234bd8d5b9e7e0deb9cfd49898
r1derPUSH <riderworkable1234@gmail.com>
2025-10-29 22:30:05 +0200

    Created basic markup for income-section component

--- src/components/MainSection/Home/IncomeSection/IncomeSection.tsx
+++ src/components/MainSection/Home/IncomeSection/IncomeSection.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function IncomeSection() {
-  return <div></div>;
+  return (
+    <div className="income-section-container">
+      <div className="income-section-top">
+        <span>Total Balance</span>
+        <span>$17,894.02</span>
+      </div>
+      <div className="income-section-bottom">
+        <div className="income-bottom-left-section"></div>
+        <div className="outcome-bottom-right-section"></div>
+      </div>
+    </div>
+  );
 }
 
 export default IncomeSection;
