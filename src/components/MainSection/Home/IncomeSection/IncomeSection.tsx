3b549fe29ea4bab8f53a3f8308b763ca96f4553a
r1derPUSH <riderworkable1234@gmail.com>
2025-10-30 23:34:52 +0200

    Changed styles for flex

--- src/components/MainSection/Home/IncomeSection/IncomeSection.tsx
+++ src/components/MainSection/Home/IncomeSection/IncomeSection.tsx
@@ -9,8 +9,18 @@ function IncomeSection() {
           <span className="total-balance-quantity">$17,894.02</span>
         </div>
         <div className="income-section-bottom">
-          <div className="income-bottom-left-section">Income</div>
-          <div className="outcome-bottom-right-section">Outcome</div>
+          <div className="income-bottom-left-section">
+            <div className="income-bottom-span-container">
+              <span>Income</span>
+              <span>$1.034,1</span>
+            </div>
+          </div>
+          <div className="outcome-bottom-right-section">
+            <div className="income-bottom-span-container">
+              <span>Outcome</span>
+              <span>$0</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
