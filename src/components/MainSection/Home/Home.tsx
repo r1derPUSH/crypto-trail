f446c3122e66c71511d9fdffd2bc4b232720ee67
r1derPUSH <riderworkable1234@gmail.com>
2025-10-31 18:22:58 +0200

    added styles for button

--- src/components/MainSection/Home/Home.tsx
+++ src/components/MainSection/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "./Header/Header";
 import IncomeSection from "./IncomeSection/IncomeSection";
+import InvestButtons from "./InvestFunctional/InvestButtons";
 
 function Home() {
   return (
     <>
       <Header />
       <IncomeSection />
+      <InvestButtons />
     </>
   );
 }
--- src/components/MainSection/Home/InvestFunctional/InvestButton/InvestButton.tsx
+++ src/components/MainSection/Home/InvestFunctional/InvestButton/InvestButton.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import investImg from "../imgs/icons8-invest-100.png";
+import "./InvestButton.css";
 
 function InvestButton() {
-  return <div></div>;
+  return (
+    <button className="send-btn">
+      <div className="img-holder">
+        <img className="img" src={investImg} alt="invest" />
+      </div>
+      <span>Invest</span>
+    </button>
+  );
 }
 
 export default InvestButton;
