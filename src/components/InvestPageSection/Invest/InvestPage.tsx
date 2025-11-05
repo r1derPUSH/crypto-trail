1d85f3b0946abe2591e32246db11c605d991011a
r1derPUSH <riderworkable1234@gmail.com>
2025-11-05 22:10:16 +0200

    Added img

--- src/components/InvestPageSection/Invest/InvestPage.tsx
+++ src/components/InvestPageSection/Invest/InvestPage.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./InvestPage.css";
+import crystalImg from "./imgs/crystal-invest.png";
 
 function InvestPage() {
-  return <div>Invest-Page</div>;
+  return (
+    <div className="invest-body">
+      <div className="invest-header">
+        <img src={crystalImg} alt="Diamond" />
+        <span>Crypto Trail</span>
+        <span>Invest</span>
+      </div>
+      <div className="invest-inputs"></div>
+      <div className="invest-button"></div>
+      <div className="back-buttons"></div>
+    </div>
+  );
 }
 
 export default InvestPage;
