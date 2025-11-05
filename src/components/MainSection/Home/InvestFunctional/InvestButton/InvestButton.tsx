015874c5be66850016c7e39c5b01a1673b2a5dbb
r1derPUSH <riderworkable1234@gmail.com>
2025-11-05 22:03:22 +0200

    Added navigation functional

--- src/components/MainSection/Home/InvestFunctional/InvestButton/InvestButton.tsx
+++ src/components/MainSection/Home/InvestFunctional/InvestButton/InvestButton.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import investImg from "../imgs/icons8-invest-100.png";
 import "./InvestButton.css";
+import { useNavigate } from "react-router-dom";
 
 function InvestButton() {
+  const navigaion = useNavigate();
+
+  const handleNavigate = () => {
+    navigaion("/invest-page-section");
+  };
+
   return (
-    <button className="send-btn">
+    <button className="send-btn" onClick={handleNavigate}>
       <div className="img-holder">
         <img className="img" src={investImg} alt="invest" />
       </div>
