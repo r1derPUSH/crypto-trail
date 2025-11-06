f02dc8b517d1bb55cb45ee80c11eefedfdfa787a
r1derPUSH <riderworkable1234@gmail.com>
2025-11-06 21:14:33 +0200

    Added basic rfce

--- src/components/ConvertPageSection/Convert/ConvertPage.tsx
+++ src/components/ConvertPageSection/Convert/ConvertPage.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function ConvertPage() {
-  return <div>Convert Page</div>;
+  return (
+    <div className="convert-header">
+      <div className="convert-header-section">
+        <img src="" alt="Swap" />
+        <span>Crypto Trail</span>
+        <span>Convert</span>
+      </div>
+      <div className="convert-section">
+        <div className="convert-from"></div>
+        <div className="convert-to"></div>
+      </div>
+      <button>Convert</button>
+      <span>Instant convertion with live rates</span>
+    </div>
+  );
 }
 
 export default ConvertPage;
