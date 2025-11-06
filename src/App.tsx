275b330f5640d833d5f59c668ccd646da36ade50
r1derPUSH <riderworkable1234@gmail.com>
2025-11-06 21:05:42 +0200

    Added basic rfce

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import MainSection from "./components/MainSection/MainSection";
 import InvestPage from "./components/InvestPageSection/Invest/InvestPage";
 import "./components/MainSection/mainSection.css";
+import ConvertPage from "./components/ConvertPageSection/Convert/ConvertPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainSection />}></Route>
         <Route path="/invest-page-section" element={<InvestPage />} />
+        <Route path="/convert-page-section" element={<ConvertPage />} />
       </Routes>
     </HashRouter>
   );
---
+++ src/components/ConvertPageSection/Convert/ConvertPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ConvertPage() {
+  return <div>Convert Page</div>;
+}
+
+export default ConvertPage;
