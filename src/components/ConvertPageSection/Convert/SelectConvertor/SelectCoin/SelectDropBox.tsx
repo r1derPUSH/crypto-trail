a0daf3d3c9942bea74b8ec65c56cb3b91c1022a6
r1derPUSH <riderworkable1234@gmail.com>
2025-11-07 19:06:06 +0200

    Created basic functional for start of dropBox functionality

--- src/components/ConvertPageSection/Convert/SelectConvertor/SelectCoin/SelectDropBox.tsx
+++ src/components/ConvertPageSection/Convert/SelectConvertor/SelectCoin/SelectDropBox.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 import "./SelectDropBox.css";
+import { useState } from "react";
 
 function SelectDropBox() {
   const [open, setOpen] = useState(false);
-  const [search, setSearch] = useState("");
+  const coins = [
+    { name: "BTC", price: 100940 },
+    { name: "ETH", price: 3350 },
+    { name: "ARB", price: 0.38 },
+    { name: "FF", price: 0.17 },
+  ];
+
+  const handleOpen = () => {
+    setOpen((prev) => !prev);
+  };
 
   return (
-    <div className="dropdown-container">
-      <button className="dropdown-btn"> ⚡ Select Currenct</button>
-    </div>
+    <>
+      <div className="dropdown-container">
+        <button onClick={handleOpen} className="dropdown-btn">
+          ⚡ Select Currenct
+        </button>
+      </div>
+
+      {open && (
+        <div className="dropdown-child">
+          <input type="text" className="dropdown-input" />
+          <ul>
+            {coins.map((item) => (
+              <li>
+                {item.name}:{item.price}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
