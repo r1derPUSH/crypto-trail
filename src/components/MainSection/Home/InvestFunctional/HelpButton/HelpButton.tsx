2e8610600e5dd3ac41e57f14e92b811f1284c9a0
r1derPUSH <riderworkable1234@gmail.com>
2025-11-08 12:47:11 +0200

    Created handleNavigate fn

--- src/components/MainSection/Home/InvestFunctional/HelpButton/HelpButton.tsx
+++ src/components/MainSection/Home/InvestFunctional/HelpButton/HelpButton.tsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import helpImg from "../imgs/icons8-help-100.png";
 import "./HelpButton.css";
 
 function HelpButton() {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    // navigate("")
+  };
+
   return (
-    <button className="help-btn">
+    <button className="help-btn" onClick={handleNavigate}>
       <div className="img-holder-help">
         <img className="img" src={helpImg} alt="invest" />
       </div>
