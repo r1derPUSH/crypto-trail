aeadd4e430a6f605a3a481c0fdbc4383ee567a12
r1derPUSH <riderworkable1234@gmail.com>
2025-11-08 13:10:30 +0200

    Created help-section

--- src/components/HelpPageSection/Help/HelpPageSection.tsx
+++ src/components/HelpPageSection/Help/HelpPageSection.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./HelpPageSection.css";
 
 function HelpPageSection() {
-  return <div>Help page section</div>;
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="help-section">
+      <h1>Help & Support</h1>
+      <p>Here you can find information about how to use Crypto Trail...</p>
+
+      <button className="back-home-btn" onClick={handleBack}>
+        ← Back to Home
+      </button>
+    </div>
+  );
 }
 
 export default HelpPageSection;
