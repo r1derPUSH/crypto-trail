ae05394dede3bca5795c4fffc311da9777432ea7
r1derPUSH <riderworkable1234@gmail.com>
2025-11-10 20:41:58 +0200

    Created new version of swapper

--- src/components/ConvertPageSection/SwapMenu/SwapFrom/SwapFrom.tsx
+++ src/components/ConvertPageSection/SwapMenu/SwapFrom/SwapFrom.tsx
@@ -6,7 +6,7 @@ function SwapFrom() {
     <div className="swap-container">
       <div className="input-price-container">
         <span className="from-span-text">From</span>
-        <input type="text" className="from-amount-input" />
+        <input type="text" className="from-amount-input" placeholder="0" />
         <span className="price-in-usd">$76.2</span>
       </div>
       <div className="dropbox-of-coins-container">
