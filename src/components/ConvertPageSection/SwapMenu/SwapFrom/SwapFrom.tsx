a12d7710da4f1c5119e99186ac31a2e834ea01e6
r1derPUSH <riderworkable1234@gmail.com>
2025-11-11 23:09:29 +0200

    Test commit

--- src/components/ConvertPageSection/SwapMenu/SwapFrom/SwapFrom.tsx
+++ src/components/ConvertPageSection/SwapMenu/SwapFrom/SwapFrom.tsx
@@ -7,7 +7,7 @@ function SwapFrom() {
       <div className="input-price-container">
         <span className="from-span-text">From</span>
         <input type="text" className="from-amount-input" placeholder="0" />
-        <span className="price-in-usd">$76.2</span>
+        <span className="price-in-usd">$76.1</span>
       </div>
       <div className="dropbox-of-coins-container">
         <div className="dropbox-of-coins"></div>
