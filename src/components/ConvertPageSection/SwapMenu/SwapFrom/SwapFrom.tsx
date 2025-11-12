863dc7c03aac4c5f9d5d243e4c13759fc5ab8a22
r1derPUSH <riderworkable1234@gmail.com>
2025-11-13 00:31:06 +0200

    Created convert section top

--- src/components/ConvertPageSection/SwapMenu/SwapFrom/SwapFrom.tsx
+++ src/components/ConvertPageSection/SwapMenu/SwapFrom/SwapFrom.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./SwapFrom.css";
+import { FaAngleDown } from "react-icons/fa";
+import ethereumImg from "../../Convert/imgs/XTVCETH--600.png";
 
 function SwapFrom() {
   return (
@@ -14,9 +16,17 @@ function SwapFrom() {
       <div className="dropbox-of-coins-container">
         <div className="dropbox-of-coins">
           <button className="dropbox-menu-inactive">
-            <img src="" alt="ETH" />
-            <span>ETH</span>
-            <img src="" alt="arrow" />
+            <img src={ethereumImg} className="coin-img" alt="ETH" />
+            <span className="coin-name">ETH</span>
+            <FaAngleDown
+              style={{
+                color: "#9AA0A6",
+                fontSize: "1.2rem",
+                backgroundColor: "rgba(0,0,0,0.05)",
+                borderRadius: "6px",
+                padding: "4px",
+              }}
+            />
           </button>
         </div>
       </div>
