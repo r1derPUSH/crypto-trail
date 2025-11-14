04430a81555e0da2f0b6eea7e6437fbc44b3f931
r1derPUSH <riderworkable1234@gmail.com>
2025-11-14 20:08:37 +0200

    Added basic markup for login-section

--- src/components/LoginPageSection/LoginSection.tsx
+++ src/components/LoginPageSection/LoginSection.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function LoginSection() {
-  return <div>Login section</div>;
+  return (
+    <>
+      <div className="login-container">
+        <div className="login-header">
+          <img src="" alt="Login" />
+          <span>Login</span>
+        </div>
+        <div className="login-fields">
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Password" />
+        </div>
+        <div className="login-buttons">
+          <button>Sign In</button>
+          <button>Register</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LoginSection;
