af69e8bd7a151b014d68ca556efed7514666be09
r1derPUSH <riderworkable1234@gmail.com>
2025-11-18 01:52:24 +0200

    Successfully added fetch

--- src/components/ConvertPageSection/Convert/ConvertPage.tsx
+++ src/components/ConvertPageSection/Convert/ConvertPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ConvertPage.css";
 import swapImg from "./imgs/swap-img.png";
 import { useNavigate } from "react-router-dom";
@@ -6,11 +6,35 @@ import SwapFrom from "../SwapMenu/SwapFrom/SwapFrom";
 import SwapTo from "../SwapMenu/SwapTo/SwapTo";
 
 function ConvertPage() {
+  const [coins, setCoins] = useState({});
   const navigate = useNavigate();
+  const [handleTime, setHandleTime] = useState();
 
   const handleNavigate = () => {
     navigate("/");
   };
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,solana"
+        );
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.error(`Error: ${err}`);
+      }
+    }
+
+    getData();
+
+    const interval = setInterval(getData, 25000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className="convert-page">
---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function useFetch({ url }) {
+  return;
+}
+
+export default useFetch;
