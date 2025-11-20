77f456fd6f9aab8fffad3df8ba2455488dfe3a69
r1derPUSH <riderworkable1234@gmail.com>
2025-11-21 00:31:01 +0200

    Added basic styles for token-section

--- src/components/InvestTokenSection/InvestCurrentTokenSectionPage/InvestToken.tsx
+++ src/components/InvestTokenSection/InvestCurrentTokenSectionPage/InvestToken.tsx
@@ -6,6 +6,28 @@ function InvestToken() {
   return (
     <div>
       <span>Invest in Token</span>
+
+      <div className="token-invest-container">
+        <div className="header">
+          <div className="first-flex-header">
+            <h1 className="token-symbol-h1">BTC</h1>
+            <span className="token-name-span">Bitcoin</span>
+          </div>
+          <div className="second-flex-header">
+            <span>89,902.05$</span>
+          </div>
+        </div>
+        <div className="invest-token-functional">
+          <input type="text" className="invest-token" />
+          <input type="text" className="recive-token" />
+        </div>
+        <div className="other-token-info">
+          <span>Price change 24H: -0.87%</span>
+          <span>TVL: 25,004,259$</span>
+          <span>Blockchain: BTC</span>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
