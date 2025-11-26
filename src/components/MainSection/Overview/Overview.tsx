ae580c9580caf8a89150e434534074e4f27a9603
r1derPUSH <riderworkable1234@gmail.com>
2025-11-27 00:01:14 +0200

    Clear

--- src/components/MainSection/Overview/Overview.tsx
+++ src/components/MainSection/Overview/Overview.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Footer from "../Footer/Footer";
+import "./Overview.css";
 
 function Overview() {
   const navigate = useNavigate();
@@ -9,11 +10,30 @@ function Overview() {
   };
 
   return (
-    <div>
-      <button onClick={handleNavigate}>Back to Home</button>
-      {/* <button></button> */}
+    <>
+      <div className="wallet-container">
+        <div className="wallet-header">
+          <span>Balance</span>
+          <span>$1,257.98</span>
+        </div>
+        <div className="wallet-buttons">
+          <button>Invest</button>
+          <button>Convert</button>
+          <button>Check PNL</button>
+        </div>
+        <div className="user-invests">
+          <div className="token-invest-position">
+            <span>ETH</span>
+            <span>PNL</span>
+          </div>
+        </div>
+        <button className="back-home-btn" onClick={handleNavigate}>
+          Back to Home
+        </button>
+        {/* <button></button> */}
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
