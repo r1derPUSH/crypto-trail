bf734a824bd8f7af86e53b9b813392d1ddc56150
r1derPUSH <riderworkable1234@gmail.com>
2025-12-19 19:33:29 +0200

    Added many functional for history-section

--- src/components/ConvertSectionHistory/ConvertSectionHistory.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ConvertSectionHistory() {
-  return <div>History</div>;
-}
-
-export default ConvertSectionHistory;
---
+++ src/components/ConvertSectionHistory/ConvertSectionHistory.tsx
@@ -0,0 +1,8 @@
+import "./ConvertSectionHistory.css";
+import HistoryItem from "./HistoryOfSwapsItem/HistoryItem";
+
+function ConvertSectionHistory() {
+  return <HistoryItem />;
+}
+
+export default ConvertSectionHistory;
