0ec363f92b5dbfca3caa202f3d192c5dac608b7b
r1derPUSH <riderworkable1234@gmail.com>
2025-12-29 19:05:22 +0200

    Changed body light-theme styles and added function to toggle theme

--- src/components/MainSection/Home/Header/ChangeTheme/ChangeTheme.tsx
+++ src/components/MainSection/Home/Header/ChangeTheme/ChangeTheme.tsx
@@ -2,9 +2,21 @@ import sunImg from "../img/icons8-sun-50.png";
 import "./ChangeTheme.css";
 
 function ChangeTheme() {
+  const changeTheme = () => {
+    const body = document.body;
+
+    if (body.classList.contains("dark-theme")) {
+      body.classList.remove("dark-theme");
+      body.classList.add("light-theme");
+    } else {
+      body.classList.remove("light-theme");
+      body.classList.add("dark-theme");
+    }
+  };
+
   return (
     <div>
-      <button className="changeTheme-button">
+      <button onClick={changeTheme} className="changeTheme-button">
         <img className="change-theme-img" src={sunImg} alt="" />
       </button>
     </div>
