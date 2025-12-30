81745aaeecc8c39d06b3ae12c2032e2403e50bfc
r1derPUSH <riderworkable1234@gmail.com>
2025-12-30 16:14:26 +0200

    Added header to other components

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,6 +1,7 @@
 import "./mainSection.css";
 import Home from "./Home/Home";
 import Footer from "./Footer/Footer";
+import Header from "./Home/Header/Header";
 
 function MainSection() {
   return (
