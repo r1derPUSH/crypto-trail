0b17e7698fb3c2501758c82c069b2460b9efd8eb
r1derPUSH <riderworkable1234@gmail.com>
2026-01-29 15:55:51 +0200

    Refactored convert-page.tsx

---
+++ src/functions/currentTime.ts
@@ -0,0 +1,12 @@
+export function getCurrentDateTime() {
+  const now = new Date();
+
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const year = now.getFullYear();
+
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+
+  return `${month}.${day}.${year} | ${hours}:${minutes}`;
+}
